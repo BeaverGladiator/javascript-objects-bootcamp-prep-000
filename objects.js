5a52ef92f72743bce1ecc8e782117cb872edbc57
Matthew Migliore <mmigliore311@gmail.com>
2018-08-17 01:42:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,6 @@
 var playlist =  {['Red Hot Chili Peppers']: 'Slow Cheetah'}
-function updatePlaylist(playlist, artistName, songTitle)
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
